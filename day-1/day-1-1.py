da741936b1a4e762ae5954a09c503fd6804fefb9
Evelyn May <kingwashboard@gmail.com>
2020-12-01 22:15:26 -0330

    did both parts of day 1

--- day-1/day-1-1.py
+++ day-1/day-1-1.py
@@ -10,4 +10,4 @@ for num1 in numbers:
     if (num2 := desired_sum - num1) in numbers:
         print(f"The two numbers which multiply to get {desired_sum} are {num1} and {num2}.")
         print(f"The product of these two numbers is {num1 * num2}.")
-        break
+        exit()
---
+++ day-1/day-1-2.py
@@ -0,0 +1,18 @@
+import sys
+
+numbers = []
+desired_sum = 2020
+
+for i in sys.stdin:
+    numbers.append(int(i))
+
+for num1 in numbers:
+
+    for num2 in numbers:
+        if numbers.index(num2) is numbers.index(num1):
+            continue
+
+        if (num3 := desired_sum - (num1 + num2)) in numbers:
+            print(f"The three numbers which multiply to get {desired_sum} are {num1}, {num2} and {num3}.")
+            print(f"The product of these three numbers is {num1 * num2 * num3}.")
+            exit()
