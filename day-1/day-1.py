95487377a9b98b620fdb6dd019a38ef3a8bda393
Evelyn May <kingwashboard@gmail.com>
2020-12-01 22:06:16 -0330

    did day 1

--- day-1/day-1.py
+++ day-1/day-1.py
@@ -6,14 +6,8 @@ desired_sum = 2020
 for i in sys.stdin:
     numbers.append(int(i))
 
-for i1 in range(len(numbers)):
-    num_index = i1
-
-    for i2 in numbers:
-        if numbers.index(i2) == num_index:
-            continue
-
-        if numbers[i1] + i2 == desired_sum:
-            print(f"The two numbers which multiply to get {desired_sum} are {numbers[i1]} and {i2}.")
-            print(f"The product of these two numbers is {numbers[i1] * i2}.")
-            exit()
+for num1 in numbers:
+    if (num2 := desired_sum - num1) in numbers:
+        print(f"The two numbers which multiply to get {desired_sum} are {num1} and {num2}.")
+        print(f"The product of these two numbers is {num1 * num2}.")
+        break
